33ca7e28d34ca6f5e2450cd1b565156ed620349d
czyjtu <maciejczyjtu@gmail.com>
2022-03-28 18:25:38 +0200

    grayscale cnn training

--- sandbox/openai/main.py
+++ sandbox/openai/main.py
@@ -8,37 +8,43 @@ from gym.spaces.discrete import Discrete
 from stable_baselines3.common.monitor import Monitor
 from datetime import datetime
 import os 
+from stable_baselines3.common.env_checker import check_env
 
-
-LOG_DIR = "./logs"
+LOG_DIR = "./logs/policy_grayscale"
+TB_DIR = LOG_DIR + "/tensorboard"
+TB_LOG_NAME = "v0"
 os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(TB_DIR, exist_ok=True)
+
 
 
 def main():
     env = Monitor(CarRacingWrapper(), filename=LOG_DIR)
+    print(check_env(env))
     now = datetime.now().strftime("%H_%M_$S")
-    save_path = LOG_DIR + "/cnn_carracing_v1" 
-    load_path = LOG_DIR + "/cnn_carracing_v0" 
+    save_path = LOG_DIR + "/cnn_carracing_gray_v1" 
+    load_path = LOG_DIR + "/best_model" 
 
-    # train(env, save_path, load=load_path)
+    train(env, save_path)
 
-    # print("TRAINING FINISHED")
+    print("TRAINING FINISHED")
 
-    watch(env, save_path)
+    # watch(env, save_path)
 
 
 def train(env, model_path, load=None):
     if load:
         model = DQN.load(load, env=env)
+        model.tensorboard_log = model.tensorboard_log or TB_DIR
         print(model)
     else:
-        model = DQN(CnnPolicy, env, verbose=1, optimize_memory_usage=False, buffer_size=100_000)
+        model = DQN(CnnPolicy, env, verbose=1, optimize_memory_usage=False, buffer_size=100_000, tensorboard_log=TB_DIR)
 
     callback = SaveOnBestTrainingRewardCallback(2000, log_dir=LOG_DIR)
 
-    model.learn(total_timesteps=1_000_000, callback=callback)
+    model.learn(total_timesteps=500_000, callback=callback, tb_log_name=TB_LOG_NAME, reset_num_timesteps=False)
     model.save(model_path)
-    del model # remove to demonstrate saving and loading
+    del model 
 
 
 def watch(env, model_path):
@@ -50,6 +56,13 @@ def watch(env, model_path):
         print(action)
         obs, rewards, dones, info = env.step(action)
         env.render()
+
+def plot():
+    from stable_baselines3.common import results_plotter
+
+    # Helper from the library
+    results_plotter.plot_results([LOG_DIR], 1e3, results_plotter.X_TIMESTEPS, "TD3 LunarLander", figsize=(10, 10))
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- sandbox/openai/wrappers/car_racing.py
+++ sandbox/openai/wrappers/car_racing.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np 
 import cv2
+from gym.spaces import Box
 
 
 class CarRacingWrapper(gym.Wrapper):
@@ -18,20 +19,30 @@ class CarRacingWrapper(gym.Wrapper):
             key: np.array(val, dtype=np.uint8) 
             for key, val in self.action_space_mapping.items()
         }
+        
+
+    @property
+    def observation_space(self):
+        return Box(0, 255, (81, 96, 1), np.uint8)
     
     @property
     def action_space(self):
         return gym.spaces.discrete.Discrete(5)
 
+    def reset(self):
+        state = self.env.reset()
+        return self.process_state(state)
+
     def step(self, action):
         action = int(action)
         action_box = self.action_space_mapping[action]
         obs, *data = self.env.step(action_box)
-        return obs, *data
+        return self.process_state(obs), *data
 
     def process_state(self, state: np.ndarray):
         clipped = np.array(state, dtype=np.uint8)[:-15, :]
         gray = cv2.cvtColor(clipped, cv2.COLOR_RGB2GRAY)
-        return gray
+        expanded = np.expand_dims(gray, -1)
+        return expanded
 
     
