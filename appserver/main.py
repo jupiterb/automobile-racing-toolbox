e90ebce52cfb2e6c0ba9e2d92c2af5a26dbe82ec
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-04-15 20:25:02 +0200

    refactoring & fixing: training manager independent from repository #2

--- appserver/main.py
+++ appserver/main.py
@@ -3,14 +3,14 @@ from fastapi.responses import PlainTextResponse
 from typing import List
 
 from schemas import Game, Training
-from repository import AbstractRepository, InMemoryRepository
+from repository import InMemoryRepository
 from training import TrainingManager
 from utils.custom_exceptions import ItemNotFound
 
 
 app = FastAPI()
-repo: AbstractRepository = InMemoryRepository()
-training_manager: TrainingManager = TrainingManager(repo)
+repo = InMemoryRepository()
+training_manager = TrainingManager()
 
 
 @app.exception_handler(ItemNotFound)
@@ -72,7 +72,8 @@ async def delete_training(game_id: str, training_id: str):
 
 @app.get("/games/{game_id}/trainings/{training_id}/run", status_code=status.HTTP_204_NO_CONTENT)
 async def run_training(game_id: str, training_id: str):
-    training_manager.run_training()
+    system_configuration, global_configuration, parameters = repo.get_full_training_configuration(game_id, training_id)
+    training_manager.run_training(system_configuration, global_configuration, )
 
 
 @app.get("/games/{game_id}/trainings/{training_id}/stop", status_code=status.HTTP_204_NO_CONTENT)
