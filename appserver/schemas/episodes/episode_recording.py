d95c45ed524bd64e75f2b0ef8dc8a9c0eb161463
czyjtu <maciejczyjtu@gmail.com>
2022-05-23 17:35:31 +0200

    fixed circular import temporarily

--- appserver/schemas/episodes/episode_recording.py
+++ appserver/schemas/episodes/episode_recording.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-from enviroments.real.interface.local import Frame
+# from enviroments.real.interface.local import Frame
 from schemas import State, Action
-
+import numpy as np 
 
 class EpisodeRecording(BaseModel):
     error: str = ""
     fps: int
-    recording: list[tuple[Frame, Action]] = []
+    recording: list[tuple[list[list[int]], Action]] = []
