c91a838d6638017114942813b9f41eef0398c899
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-08-04 01:35:39 +0200

    fixes: event detector tests with fixtures

--- racing_toolbox/tests/test_event_detector.py
+++ racing_toolbox/tests/test_event_detector.py
@@ -18,24 +18,29 @@ from conf import (
 )
 
 
-def test_detector_not_accept_unvalid_parameters() -> None:
-    parameters = EventDetectionParameters(
-        feature_name="test",
-        min_value=10.0,
-        max_value=0.0,
-        required_repetitions_in_row=3,
-        different_values_required=0,
-    )
-    with pytest.raises(ValueError):
-        detector = EventDetector([parameters])
-    parameters.max_value = 20
-    parameters.different_values_required = -1
-    with pytest.raises(ValueError):
-        detector = EventDetector([parameters])
+@pytest.fixture
+def unvalid_event_detector_parameters() -> list[EventDetectionParameters]:
+    return [
+        EventDetectionParameters(
+            feature_name="min_value_greater_than_max_value",
+            min_value=10.0,
+            max_value=0.0,
+            required_repetitions_in_row=3,
+            different_values_required=0,
+        ),
+        EventDetectionParameters(
+            feature_name="negative_different_values_required",
+            min_value=0.0,
+            max_value=10.0,
+            required_repetitions_in_row=3,
+            different_values_required=-1,
+        ),
+    ]
 
 
-def test_event_detection() -> None:
-    parameters = [
+@pytest.fixture
+def valid_event_detector_parameters() -> list[EventDetectionParameters]:
+    return [
         EventDetectionParameters(
             feature_name="speed",
             min_value=float("-inf"),
@@ -51,7 +56,59 @@ def test_event_detection() -> None:
             different_values_required=0,
         ),
     ]
-    detector = EventDetector(parameters)
+
+
+@pytest.fixture
+def end_of_race_screenshots() -> list[np.ndarray]:
+    path_to_images = "assets/screenshots/end_of_race"
+    return [
+        np.array(Image.open(f"{path_to_images}/{name}"))
+        for name in listdir(path_to_images)
+    ]
+
+
+@pytest.fixture
+def checkpoint_screenshots() -> list[np.ndarray]:
+    path_to_images = "assets/screenshots/checkpoint"
+    return [
+        np.array(Image.open(f"{path_to_images}/{name}"))
+        for name in listdir(path_to_images)
+    ]
+
+
+def perform_detection_on_real_images(
+    images: list[np.ndarray], detector: EventDetector, monkeypatch
+) -> None:
+    detector.reset()
+    assert not detector.is_final()
+
+    event_detected = False
+    interface = TrainingLocalGameInterface(get_game_config())
+
+    for image in images:
+
+        def mock_get_screenshot(*args, **kwargs):
+            return image
+
+        monkeypatch.setattr(Screen, "_get_screenshot", mock_get_screenshot)
+        if detector.is_final(interface.perform_ocr(on_last_image=False)):
+            event_detected = True
+            detector.reset()
+
+    assert event_detected
+    assert not detector.is_final()
+
+
+def test_detector_not_accept_unvalid_parameters(
+    unvalid_event_detector_parameters,
+) -> None:
+    for parameters in unvalid_event_detector_parameters:
+        with pytest.raises(ValueError):
+            _ = EventDetector([parameters])
+
+
+def test_event_detection(valid_event_detector_parameters) -> None:
+    detector = EventDetector(valid_event_detector_parameters)
 
     assert not detector.is_final({"speed": 0.0, "last_checkpoint_detected": 0.0})
     assert not detector.is_final({"speed": 0.0, "last_checkpoint_detected": 0.0})
@@ -72,46 +129,11 @@ def test_event_detection() -> None:
     assert not detector.is_final()
 
 
-def test_final_state_detection(monkeypatch) -> None:
+def test_final_state_detection(monkeypatch, end_of_race_screenshots) -> None:
     detector = EventDetector(get_final_state_detection_parameters())
-
-    assert not detector.is_final()
-
-    end_of_race_detected = False
-    interface = TrainingLocalGameInterface(get_game_config())
-
-    path_to_images = "assets/screenshots/end_of_race"
-    for image_name in listdir(path_to_images):
-
-        def mock_get_screenshot(*args, **kwargs):
-            return np.array(Image.open(f"{path_to_images}/{image_name}"))
-
-        monkeypatch.setattr(Screen, "_get_screenshot", mock_get_screenshot)
-        end_of_race_detected = detector.is_final(
-            interface.perform_ocr(on_last_image=False)
-        )
-
-    assert end_of_race_detected
+    perform_detection_on_real_images(end_of_race_screenshots, detector, monkeypatch)
 
 
-def test_checkpoint_detection(monkeypatch) -> None:
+def test_checkpoint_detection(monkeypatch, checkpoint_screenshots) -> None:
     detector = EventDetector(get_checkpoint_detection_parameters())
-
-    assert not detector.is_final()
-
-    checpoint_detected = False
-    interface = TrainingLocalGameInterface(get_game_config())
-
-    path_to_images = "assets/screenshots/checkpoint"
-    for image_name in listdir(path_to_images):
-
-        def mock_get_screenshot(*args, **kwargs):
-            return np.array(Image.open(f"{path_to_images}/{image_name}"))
-
-        monkeypatch.setattr(Screen, "_get_screenshot", mock_get_screenshot)
-        if detector.is_final(interface.perform_ocr(on_last_image=False)):
-            checpoint_detected = True
-            detector.reset()
-
-    assert checpoint_detected
-    assert not detector.is_final()
+    perform_detection_on_real_images(checkpoint_screenshots, detector, monkeypatch)
