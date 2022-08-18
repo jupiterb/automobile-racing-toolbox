3d9191982955384461c2d32615d47ca77a96b7db
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-08-18 23:54:08 +0200

    gamepad control enum values

--- racing_toolbox/interface/models/gamepad_action.py
+++ racing_toolbox/interface/models/gamepad_action.py
@@ -2,13 +2,16 @@ from vgamepad import XUSB_BUTTON
 from enum import Enum
 
 
+max_xusb = max(XUSB_BUTTON)
+
+
 class GamepadControl(Enum):
-    LEFT_TRIGGER = 0
-    RIGHT_TRIGGER = 1
-    LEFT_JOYSTICK_X = 2
-    LEFT_JOYSTICK_Y = 3
-    RIGHT_JOYSTICK_X = 4
-    RIGHT_JOYSTICK_Y = 5
+    LEFT_TRIGGER = max_xusb + 1
+    RIGHT_TRIGGER = max_xusb + 2
+    LEFT_JOYSTICK_X = max_xusb + 3
+    LEFT_JOYSTICK_Y = max_xusb + 4
+    RIGHT_JOYSTICK_X = max_xusb + 5
+    RIGHT_JOYSTICK_Y = max_xusb + 6
 
 
 GamepadAction = GamepadControl | XUSB_BUTTON
