4f90420a0b746bea92c66d75b082d3efafb56b36
czyjtu <maciejczyjtu@gmail.com>
2022-09-09 14:35:01 +0200

    create init file for racing_toolbox module

--- racing_toolbox/logger.py
+++ racing_toolbox/logger.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import logging.config
-import logging 
+import logging
 import yaml
 
 
-def setup_applevel_logger(logger_config_path: Path) -> None:
+def setup_logger(logger_config_path: Path) -> None:
     """
     Function initializes logger for the application from provided config file
 
@@ -12,8 +12,8 @@ def setup_applevel_logger(logger_config_path: Path) -> None:
         logger_config_path (Path): path to configuration file for logging
 
     """
-    
-    with open(logger_config_path, 'rt') as f:
+
+    with open(logger_config_path, "rt") as f:
         config = yaml.safe_load(f.read())
-    
+
     logging.config.dictConfig(config)
