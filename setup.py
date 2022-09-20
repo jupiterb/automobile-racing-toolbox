0b37fa8cea4d632c8b0fa8f63704dd492f75c82c
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-09-20 20:02:32 +0200

    update requirements and create setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+requirements = []
+
+setup(
+    name="Racing Toolbox",
+    version="0.1.0",
+    description="Racing Toolbox - module for RL for racing games",
+    packages=find_packages(),
+    install_requires=requirements,
+)
