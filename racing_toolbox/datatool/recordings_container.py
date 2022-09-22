abb01dd7885ad4347a5ba42c552c469d8781af09
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-09-22 18:51:15 +0200

    recordings container + tests

--- racing_toolbox/datatool/recordings_container.py
+++ racing_toolbox/datatool/recordings_container.py
@@ -15,7 +15,16 @@ class RecordingsContainer:
         return False
 
     def can_be_added(self, recording: Recording) -> bool:
-        return False
+        if not any(self._recordings):
+            return True
+        representant = self._recordings[0]
+        return (
+            representant.game == recording.game
+            and representant.fps == recording.fps
+            and representant.images[0].shape == recording.images[0].shape
+            and representant.features[0].shape == recording.features[0].shape
+            and representant.actions[0].shape == recording.actions[0].shape
+        )
 
     def get_all(
         self,
