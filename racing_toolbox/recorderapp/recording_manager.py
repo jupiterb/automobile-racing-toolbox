f460ee2ee0a784af5a81a150bf95db8b85ab3816
Piotr Bialy <piotr.bialy.407@gmail.com>
2022-09-28 17:59:33 +0200

    Recording manager: smart delay to meet expected fps

--- racing_toolbox/recorderapp/recording_manager.py
+++ racing_toolbox/recorderapp/recording_manager.py
@@ -1,12 +1,17 @@
-import threading
-from typing import Optional, Callable
+import logging
 import time
+import threading
+
+from contextlib import contextmanager
+from typing import Optional, Callable
 
 from racing_toolbox.interface import GameInterface
 from racing_toolbox.datatool.services import (
     InMemoryDatasetService,
     AbstractDatasetService,
 )
+
+logger = logging.getLogger(__name__)
 
 
 class EpisodeRecordingManager:
@@ -59,11 +64,35 @@ class EpisodeRecordingManager:
         fps: int,
     ) -> None:
         game_interface.reset()
+        expected_capture_duartion = 1 / fps
         with self.__get_dataservice() as service:
             while self.__running:
-                if self.__capturing:
-                    image = game_interface.grab_image()
-                    action = game_interface.read_action()
-                    service.put(image, action)
-                    time.sleep(1 / fps)
+                self.__capture_if_needed(
+                    game_interface, service, expected_capture_duartion
+                )
         game_interface.reset(False)
+
+    def __capture_if_needed(
+        self,
+        game_interface: GameInterface,
+        service: AbstractDatasetService,
+        expected_duartion: float,
+    ):
+        if self.__capturing:
+            with EpisodeRecordingManager.__expect_time_duration(expected_duartion):
+                image = game_interface.grab_image()
+                action = game_interface.read_action()
+                service.put(image, action)
+
+    @contextmanager
+    @staticmethod
+    def __expect_time_duration(expected_duration: float):
+        start = time.time()
+        yield None
+        duration = time.time() - start
+        if expected_duration >= duration:
+            time.sleep(expected_duration - duration)
+        else:
+            logging.warning(
+                f"Duration of the task ({duration}s) was greater than expected ({expected_duration}s)"
+            )
