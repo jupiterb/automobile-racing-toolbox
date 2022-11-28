8a037a811255563cde0387c3341bb3fbe98e7955
czyjtu <maciejczyjtu@gmail.com>
2022-11-28 15:18:50 +0100

    small test adjustments

--- tests/test_training/test_wrappers.py
+++ tests/test_training/test_wrappers.py
@@ -1,11 +1,11 @@
 import pytest
-from racing_toolbox.environment.wrappers.vae import load_vae_from_wandb_checkpoint 
-import torch as th 
+from racing_toolbox.environment.wrappers.vae import load_vae_from_wandb_checkpoint
+import torch as th
 
 
-@pytest.fixture 
+@pytest.fixture
 def vae_latent8_path():
-    return "automobile-racing-toolbox/VAEv2/model-1nj63s66:v110"
+    return "automobile-racing-toolbox/VAEv2/model-1g5v58m2:v149"
 
 
 def test_model_loaded_properly(vae_latent8_path):
@@ -16,6 +16,5 @@ def test_model_loaded_properly(vae_latent8_path):
 
     sample = th.zeros((1, 3, 128, 128))
     with th.no_grad():
-        latent = model.to_latent(sample).detach().numpy()
-    assert latent.shape == (8, ), "Invalid latent vector shape"
-    
+        latent = model.to_latent(sample).detach().squeeze(0).numpy()
+    assert latent.shape == (8,), "Invalid latent vector shape"
