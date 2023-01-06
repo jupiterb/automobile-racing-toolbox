885e0d0445cbe319c47a96f3ed2cc276c202c3c0
Piotr Bialy <piotr.bialy.407@gmail.com>
2023-01-06 15:54:28 +0100

    fix lidar wrapper

--- common/racing_toolbox/environment/wrappers/observation.py
+++ common/racing_toolbox/environment/wrappers/observation.py
@@ -2,9 +2,9 @@ import gym
 import numpy as np
 import wandb
 import gym.spaces
-import torch as th 
-from torchvision import transforms 
-import logging 
+import torch as th
+from torchvision import transforms
+import logging
 
 from racing_toolbox.observation.config import LidarConfig, TrackSegmentationConfig
 from racing_toolbox.observation.lidar import Lidar
@@ -25,16 +25,14 @@ class VaeObservationWrapper(gym.ObservationWrapper):
         self.observation_space = gym.spaces.Box(
             low=np.finfo(np.float32).min,
             high=np.finfo(np.float32).max,
-            shape=(self.vae.latent_dim, )
+            shape=(self.vae.latent_dim,),
         )
-        transform_list = [
-            transforms.ToTensor(),
-            transforms.Resize(vae.input_shape)
-        ]
-        if vae.in_channels == 1: 
+        transform_list = [transforms.ToTensor(), transforms.Resize(vae.input_shape)]
+        if vae.in_channels == 1:
             transform_list.append(transforms.Grayscale())
-        self.transform = transforms.Compose(transform_list) # TODO: transforms stuff should be passed in constructor, or be implemented in vae class to avoid duplication 
-
+        self.transform = transforms.Compose(
+            transform_list
+        )  # TODO: transforms stuff should be passed in constructor, or be implemented in vae class to avoid duplication
 
     def observation(self, observation: np.ndarray) -> np.ndarray:
         """given RGB compatible with vae model input, return sample from latent space"""
@@ -53,7 +51,8 @@ class SqueezingWrapper(gym.ObservationWrapper):
         self.move_axis = lambda obs: np.moveaxis(obs, 0, -1)
         self.observation_space = gym.spaces.Box(
             np.min(env.observation_space.low),
-            np.max(env.observation_space.high), self.move_axis(env.observation_space.sample()).shape
+            np.max(env.observation_space.high),
+            self.move_axis(env.observation_space.sample()).shape,
         )
 
     @log_observation(__name__)
@@ -65,7 +64,9 @@ class SqueezingWrapper(gym.ObservationWrapper):
 class RescaleWrapper(gym.ObservationWrapper):
     def __init__(self, env: gym.Env) -> None:
         super().__init__(env)
-        self.observation_space = gym.spaces.Box(0, 1, env.observation_space.sample().shape)
+        self.observation_space = gym.spaces.Box(
+            0, 1, env.observation_space.sample().shape
+        )
 
     @log_observation(__name__)
     def observation(self, observation: np.ndarray):
@@ -75,12 +76,10 @@ class RescaleWrapper(gym.ObservationWrapper):
 class CutImageWrapper(gym.ObservationWrapper):
     def __init__(self, env: gym.Env, frame: ScreenFrame) -> None:
         super().__init__(env)
-        self._frame = frame 
+        self._frame = frame
         shape = frame.apply(env.observation_space.sample()).shape
         self.observation_space = gym.spaces.Box(
-            np.min(env.observation_space.low),
-            np.max(env.observation_space.high),
-            shape
+            np.min(env.observation_space.low), np.max(env.observation_space.high), shape
         )
 
     @log_observation(__name__)
@@ -93,26 +92,28 @@ class LidarWrapper(gym.ObservationWrapper):
         super().__init__(env)
         self._lidar = Lidar(config)
         self.observation_space = gym.spaces.Box(
-            0, 1, (len(range(*config.angles_range)) + 1, config.depth)
+            0, 1, ((len(range(*config.angles_range)) + 1) * config.depth,)
         )
 
     @log_observation(__name__)
     def observation(self, observation: np.ndarray):
-        return self._lidar.scan_2d(observation)[0]
+        return self._lidar.scan_2d(observation)[0].flatten()
 
 
 class TrackSegmentationWrapper(gym.ObservationWrapper):
     def __init__(self, env: gym.Env, config: TrackSegmentationConfig) -> None:
         super().__init__(env)
         self._track_segmenter = TrackSegmenter(config)
-        # TODO(jupiterb): add observation space 
+        # TODO(jupiterb): add observation space
 
     @log_observation(__name__)
     def observation(self, observation: np.ndarray) -> np.ndarray:
         return self._track_segmenter.perform_segmentation(observation)
 
-def log_video(imgs: list[np.ndarray], key_name: str="recording"):
+
+def log_video(imgs: list[np.ndarray], key_name: str = "recording"):
     wandb.run.log({key_name: wandb.Video(np.stack(imgs), fps=10)})
+
 
 class WandbVideoLogger(gym.Wrapper):
     def __init__(self, env: gym.Env, log_frequency: int, log_duration: int) -> None:
@@ -127,7 +128,6 @@ class WandbVideoLogger(gym.Wrapper):
         self._step = 0
 
         self.pool = ThreadPoolExecutor(max_workers=10)
-
 
     def step(self, action):
         obs, rew, done, info = super().step(action)
@@ -144,7 +144,7 @@ class WandbVideoLogger(gym.Wrapper):
             self._frames = []
 
     def _record(self, obs: np.ndarray):
-        img = np.moveaxis(obs, -1, 0) # channel first
+        img = np.moveaxis(obs, -1, 0)  # channel first
         self._frames.append(img)
         if self.log_duration == len(self._frames):
             logger.info("logging video")
@@ -153,16 +153,20 @@ class WandbVideoLogger(gym.Wrapper):
 
 
 class VaeVideoLogger(WandbVideoLogger):
-    def __init__(self, env: gym.Env, log_frequency: int, log_duration: int, vae: VanillaVAE, decode_only: bool=False, ) -> None:
+    def __init__(
+        self,
+        env: gym.Env,
+        log_frequency: int,
+        log_duration: int,
+        vae: VanillaVAE,
+        decode_only: bool = False,
+    ) -> None:
         """if decode_only is set, will assume that given observation is latent vector, and use only Decoder to log frame"""
         super().__init__(env, log_duration=log_duration, log_frequency=log_frequency)
         self.vae = vae
         self.vae.eval()
-        transform_list = [
-            transforms.ToTensor(),
-            transforms.Resize(vae.input_shape)
-        ]
-        if vae.in_channels == 1: 
+        transform_list = [transforms.ToTensor(), transforms.Resize(vae.input_shape)]
+        if vae.in_channels == 1:
             transform_list.append(transforms.Grayscale())
         self.transform = transforms.Compose(transform_list)
         self.decode_only = decode_only
@@ -184,6 +188,3 @@ class VaeVideoLogger(WandbVideoLogger):
             logger.info("logging VAE video")
             self.pool.submit(log_video, self._frames, key_name="vae_reconstruction")
             logger.info("logged VAE video")
-    
-    
-
